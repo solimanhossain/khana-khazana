7bcfe39f2143d9dae64e9fba84c68c980f091e4d
solimanhossain <solimanhossainsohan@gmail.com>
2024-10-04 01:41:43 +0600

    Update

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -5,20 +5,28 @@ export default function RecipeCard({ recipe }) {
     const { name, thumbnail, rating, author } = recipe;
 
     return (
-        <Link className="card" key={recipe._id} href={`/details/${recipe._id}`}>
-            <Image
-                src={thumbnail}
-                className="rounded-md"
-                alt="card image"
-                width={300}
-                height={160}
-            />
-            <h4 className="my-2">{name}</h4>
-            <div className="py-2 flex justify-between text-xs text-gray-500">
-                <span>
-                    {Array(rating).fill("⭐️")} {rating}
-                </span>
-                <span>By: {author}</span>
+        <Link
+            className="card border rounded-md"
+            key={recipe._id}
+            href={`/details/${recipe._id}`}
+        >
+            <div className="relative h-[180px] overflow-hidden">
+                <Image
+                    src={thumbnail}
+                    alt="card image"
+                    width={400}
+                    height={180}
+                    objectFit="cover"
+                />
+            </div>
+            <div className="m-4">
+                <h4 className="my-2">{name}</h4>
+                <div className="py-2 flex justify-between text-xs text-gray-500">
+                    <span>By: {author}</span>
+                    <span>
+                        {Array(rating).fill("⭐️")} {rating}
+                    </span>
+                </div>
             </div>
         </Link>
     );
